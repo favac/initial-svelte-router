1633a5b9ecc181545fd7f0377e132a4e65a8001c
favac <felix.vargas@gmail.com>
2020-06-15 11:48:15 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  // import NavLink from "./components/NavLink.svelte";
+  import NavBar from "./components/NavBar.svelte";
+  import Home from "./routes/Home.svelte";
+  import About from "./routes/About.svelte";
+  import Blog from "./routes/Blog.svelte";
+
+  // Used for SSR. A falsy value is ignored by the Router.
+  export let url = "";
+</script>
+
+<Router {url}>
+  <NavBar />
+  <div class="container">
+    <Route path="about" component={About} />
+    <Route path="blog/*" component={Blog} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
